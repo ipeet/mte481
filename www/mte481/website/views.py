dc77285fc5f275bdbe51bc145fed75759578a03b
Iain Peet <iain@ipeet.org>
2012-01-21 14:07:37 -0500

    Added templated stylesheet, for easy parameterization

--- www/mte481/website/views.py
+++ www/mte481/website/views.py
@@ -2,6 +2,13 @@
 from django.http import HttpResponse, Http404
 from django.shortcuts import render_to_response
 
+def root(request):
+  """ Requests for the root are just redirected to the about page """
+  resp = HttpResponse()
+  resp.status_code = 301
+  resp['Location'] = "http://"+request.get_host()+"/about"
+  return resp
+
 # A list of available pages.  
 # The key 'page' specifies the url the page serves under, and also identifies 
 # the page template.
@@ -12,18 +19,9 @@ page_list = [
   {'name': 'group', 'title': 'Team Members'},
 ]
 
-def root(request):
-  """ Requests for the root are just redirected to the about page """
-  resp = HttpResponse()
-  resp.status_code = 301
-  resp['Location'] = "http://"+request.get_host()+"/about"
-  return resp
-
 def page(request, page):
   """ Handles requests for the various pages given in page_list """
-
-  if not page in [p['name'] for p in page_list]:
-    raise Http404
+  print("page: "+page)
 
   # Find the title that goes with the page 
   title = None
@@ -31,6 +29,34 @@ def page(request, page):
     if p['name'] == page:
       title = p['title']
 
+  if title is None:
+    raise Http404
+
+
   return render_to_response(page+'.html', 
       {'name': page, 'title': title, 'page_list': page_list})
+
+# A list of available stylesheet templates
+style_list = [
+  "main",
+]
+
+# Parameters for stylesheets to use:
+style_args = {
+  'palette': {
+    'background': '#444',
+  },
+}
+
+def style(request, style):
+  """ Handles requests for stylesheet templates """
+  print("style: "+style)
+
+  # Ensure the requested style exists:
+  if not style in style_list:
+    raise Http404
+
+  response = render_to_response(style+'.css', style_args);
+  response['Content-Type'] = 'text/css; charset=utf-8'
+  return response
 
