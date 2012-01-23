b8ba045c8c3ba2a12b4759f30d5a65f0d39c3a09
Iain Peet <iain@ipeet.org>
2012-01-22 20:23:26 -0500

    Final tweaks of CSS

--- www/mte481/website/views.py
+++ www/mte481/website/views.py
@@ -22,8 +22,6 @@ page_list = [
 
 def page(request, page):
   """ Handles requests for the various pages given in page_list """
-  print("page: "+page)
-
   # Find the title that goes with the page 
   title = None
   for p in page_list:
@@ -41,34 +39,42 @@ style_list = [
   "main",
 ]
 
-# Parameters for stylesheets to use:
-style_args = {
-  'palette': {
-    'bg': '#6e84d6',
-    'bg_head': '#4867d6',
-    'bg_head_content': '#1435ad',
-    'bg_text': '#fff',
-    'bg_nav': '#2c3e82',
-    'text': '#000',
-    'text_head': '#fff',
-    'text_nav': '#fff',
-    'heading': '#ffb100',
-    'heading_text': '#000',
-    'border': '#ffd473',
-  },
-  'page_width': '70%',
-  'head_height': '105px',
+# Colour palatte used in stylesheet:
+style_palette = {
+  'bg': '#6e84d6',
+  'bg_head': '#4867d6',
+  'bg_head_content': '#1435ad',
+  'bg_text': '#fff',
+  'bg_nav': '#2c3e82',
+  'text': '#000',
+  'text_head': '#fff',
+  'text_nav': '#fff',
+  'heading': '#ffb100',
+  'heading_text': '#000',
+  'border': '#ffd473',
 }
 
 def style(request, style):
   """ Handles requests for stylesheet templates """
-  print("style: "+style)
 
   # Ensure the requested style exists:
   if not style in style_list:
     raise Http404
 
-  response = render_to_response(style+'.css', style_args);
+  # Disable fixed UI for mobile UAs, since they don't handle it well.
+  fixedUi = True
+  if request.META['HTTP_USER_AGENT'].find('Android') != -1:
+    fixedUi = False
+  if request.META['HTTP_USER_AGENT'].find('iPhone') != -1:
+    fixedUi = False
+  print(type(request.META['HTTP_USER_AGENT']))
+
+  response = render_to_response(style+'.css', {
+    'palette': style_palette, 
+    'page_width': '70%',
+    'head_height': '105px',
+    'fixed_ui': fixedUi,
+  })
   response['Content-Type'] = 'text/css; charset=utf-8'
   return response
 
