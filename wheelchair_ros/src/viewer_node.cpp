e3a68b381ad24b2f0a3ef03d73509ed345d038f5
Iain Peet <iain@ipeet.org>
2012-03-06 18:39:23 -0500

    Viewer node subscribed to map

--- wheelchair_ros/src/viewer_node.cpp
+++ wheelchair_ros/src/viewer_node.cpp
@@ -4,8 +4,16 @@
 #include <GL/glu.h>
 #include <GL/glut.h>
 
+#include <ros/ros.h>
+#include <nav_msgs/OccupancyGrid.h>
+#include <iostream>
+
+using namespace std;
+using namespace nav_msgs;
+
 const int WIDTH = 600;
 const int HEIGHT = 400;
+const int TICK_MS = 100;
 
 void render() {
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -19,13 +27,34 @@ void render() {
   glutSwapBuffers();
 }
 
+void occupancyCallback(const OccupancyGrid::ConstPtr &msg) {
+}
+
+/* Called at 20Hz.  Does ROS work, among other things. */
+void tick(int value) {
+  glutTimerFunc(TICK_MS, tick, 0);
+  if (!ros::ok()) {
+    exit(0);
+  }
+  ros::spinOnce();
+}
+
 int main(int argc, char *argv[]) {
   glutInit(&argc, argv);
+
+  /* ROS init */
+  ros::init(argc, argv, "viewer_node");
+  ros::NodeHandle nh;
+  ros::Subscriber sub = nh.subscribe<OccupancyGrid>(
+      "map", 1, occupancyCallback);
+
+  /* GLUT init */
   glutInitWindowPosition(0, 0);
   glutInitWindowSize(WIDTH, HEIGHT);
   glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
   glutCreateWindow("Wheelchar 482");
   glutDisplayFunc(render);
+  glutTimerFunc(TICK_MS, tick, 0);
   glutMainLoop();
   return 0;
 }
