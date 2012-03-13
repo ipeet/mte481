fcd6a8dcf4794adcf9ffc59eec25b7a38a53ae85
Iain Peet <iain@ipeet.org>
2012-03-13 11:09:42 -0400

    Add a simple test data publisher.

---
+++ wheelchair_ros/src/test_publisher.cpp
@@ -0,0 +1,24 @@
+/* Ros node which publishes data used for the testing of other nodes */
+
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+
+using geometry_msgs::Twist;
+
+int main(int argc, char *argv[]) {
+  ros::init(argc, argv, "test_publisher");
+  ros::NodeHandle node;
+
+  ros::Publisher wheelJs (node.advertise<Twist>("wheel_js_in", 1));
+
+  ros::Rate loopRate(5);
+  while (ros::ok()) {
+    Twist::Ptr js (new Twist);
+    js->linear.x = 0.1;
+    js->linear.y = 0.5;
+    wheelJs.publish(js);
+    
+    loopRate.sleep();
+  }
+  return 0;
+}
